6c76fb682f138b95f95d68a6843e24b69a76d1be
Michael Zeuner <mzeuner@axyz.com>
2020-12-09 20:36:45 -0500

    Add: ability to set the submitted_by value

--- CRUD/report-cards.php
+++ CRUD/report-cards.php
@@ -30,38 +30,26 @@ class ReportCards extends CRUD
     }
 
     protected function dataManipulationUpdate($data) { 
-        if (isset($data['level'])) {
-            $newData['athletes_id'] = $data['athlete']['id'];
-            $newData['secondary_coach_id'] = $data['secondary_coach_id'];
-            $newData['levels_id'] = $data['level']['id'];
-            $newData['comment'] = $data['comment'];
-            $newData['approved'] = $data['approved'];
-            $newData['status'] = $data['status'];
-            $newData['session'] = $data['session'];
-            $newData['day_of_week'] = $data['day_of_week'];
-            return $newData; 
-        }
-        else {
-            $newData['athletes_id'] = $data['athletes_id'];
-            $newData['secondary_coach_id'] = $data['secondary_coach_id'];
-            $newData['levels_id'] = $data['levels_id'];
-            $newData['comment'] = $data['comment'];
-            $newData['approved'] = $data['approved'];
-            $newData['status'] = $data['status'];
-            $newData['session'] = $data['session'];
-            $newData['day_of_week'] = $data['day_of_week'];
-            return $newData;
-        }
+        $newData['submitted_by'] = $data['submitted_by'];
+        $newData['athletes_id'] = $data['athlete']['id'];
+        $newData['secondary_coach_id'] = $data['secondary_coach_id'];
+        $newData['levels_id'] = $data['level']['id'];
+        $newData['comment'] = $data['comment'];
+        $newData['approved'] = $data['approved'];
+        $newData['status'] = $data['status'];
+        $newData['session'] = $data['session'];
+        $newData['day_of_week'] = $data['day_of_week'];
+        return $newData; 
     }
 
     protected function getRequiredUpdateData() {
-        return ['athletes_id', 'secondary_coach_id', 'levels_id', 'comment', 'approved', 'status', 'session', 'day_of_week'];
+        return ['submitted_by', 'athletes_id', 'secondary_coach_id', 'levels_id', 'comment', 'approved', 'status', 'session', 'day_of_week'];
     }
     
     protected function getUpdateSQL() {
         $date = $this->getCurrentDateTime();
         return 'UPDATE '.$this->getTableName().
-                " SET athletes_id = :athletes_id, secondary_coach_id = :secondary_coach_id, levels_id = :levels_id, comment = :comment, approved = :approved, status = :status, session = :session, day_of_week = :day_of_week, updated_date = '$date'".
+                " SET submitted_by = :submitted_by, athletes_id = :athletes_id, secondary_coach_id = :secondary_coach_id, levels_id = :levels_id, comment = :comment, approved = :approved, status = :status, session = :session, day_of_week = :day_of_week, updated_date = '$date'".
                 ' WHERE id = :id';
     }
 
